80f7991c67c1b91fe1874345dfeafda1c3bd1e00
MDRayyan2006 <studynow12463@gmail.com>
2025-06-04 22:44:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from scraper import get_recommendations
-
+import os 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -13,5 +13,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
